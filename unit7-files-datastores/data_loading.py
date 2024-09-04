a2d4945ccd31af211002bb1d8d5cdc5f9315259b
Jacob Li <lijacob290@gmail.com>
2024-09-04 17:35:32 +0000

    more stuff

--- unit7-files-datastores/data_loading.py
+++ unit7-files-datastores/data_loading.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import csv
 
 def setup_sql_table(user, password, host, new_db, new_table):
     conn = mysql.connector.connect(
@@ -11,7 +12,7 @@ def setup_sql_table(user, password, host, new_db, new_table):
     create_statement = f'CREATE DATABASE {new_db};'
     create_table = f'''
         CREATE TABLE {new_db}.{new_table} (
-            id VARCHAR(255) PRIMARY KEY,
+            id VARCHAR(255) NOT NULL PRIMARY KEY,
             title VARCHAR(1024),
             authors VARCHAR(1024),
             average_rating FLOAT(9, 2),
@@ -23,7 +24,7 @@ def setup_sql_table(user, password, host, new_db, new_table):
             text_reviews_count INT,
             publication_date VARCHAR(255),
             publisher VARCHAR(255)
-        )
+        );
     '''
     cur = conn.cursor()
     cur.execute(del_statement)
@@ -39,9 +40,15 @@ def load_to_sql(user, password, host, db, table, csv_in):
         user=user,
         password=password
     )
-    cur = conn.cursor()
-    insert_statement = f'''
-        INSERT INTO {db}.{table} ()'''
-
+    with open(csv_in, newline='') as csv_file:
+        csv_dict_reader = csv.DictReader(csv_file)
+        for row in csv_dict_reader:
+            cur = conn.cursor()
+            insert_statement = f'''
+                INSERT INTO {db}.{table}
+                VALUES ({row["bookID"]}, {row["title"]}, {row["authors"]}, {row["average_rating"]}, {row["isbn"]}, {row["isbn13"]}, {row["language_code"]}, {row["num_pages"]}, {row["ratings_count"]}, {row["text_reviews_count"]}, {row["publication_date"]}, {row["publisher"]})'''
+            cur.execute(insert_statement)
+            cur.close()
+            conn.commit()
 if __name__ == "__main__":
     setup_sql_table('dbuser', 'dbroot', 'localhost', 'books', 'goodreads')
