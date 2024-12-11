15561538ba71b77ce211525cfbe3d9b5806c72ed
Jacob Li <lijacob290@gmail.com>
2024-12-11 10:29:54 -0700

    pascal's done

--- pascal.py
+++ pascal.py
@@ -1,24 +1,35 @@
-def get_pascal_row(row_number):
-    if row_number == 1:
-        return "1"
-    if row_number == 2:
-        return "1 1"
-    
-    """
-    2 5
-    row_number = 3; 1 2 1
-    row_number = 4; 1 3 3 1'
-    """
+def combination_theorem(row_num):
+    answers = []
+    # row 4 -> 3c0, 3c1, 3c2, 3c3
+    for i in range(0, row_num):
+        top = factorial(row_num - 1)
+        bottom = factorial(i) * factorial(row_num - 1 - i)
+        answers.append(int(top/bottom))
+    return answers
 
-    numbers = [1, 1]
-    new_numbers = [1, 1]
-    if (len(numbers) < row_number):
-        for i in range(0, row_number - 3):
-            sum = numbers[i] + numbers[i + 1]
-            numbers_left = new_numbers[0:i + 1]
-            numbers_right = new_numbers[i + 1:]
-            new_numbers = numbers_left + [sum] + numbers_right
-    return numbers
+def factorial(num):
+    if num == 0:
+        return 1
+    elif num == 1:
+        return 1
+    else:
+        return num * factorial(num - 1)
+
+def format_row(row):
+    row = [str(x) for x in row]
+    return " ".join(row)
     
 if __name__ == "__main__":
-    print(get_pascal_row(4))
+    rows = []
+    row = int(input("Select a height: "))
+    if row >= 1:
+        rows.append([1])
+    if row >= 2:
+        rows.append([1, 1])
+    # this is for row 7
+    for i in range(3, row + 1):
+        rows.append(combination_theorem(i))
+    max_length = len(format_row(rows[-1]))
+    
+    for i in range(0, len(rows)):
+        print(format_row(rows[i]).center(max_length))
